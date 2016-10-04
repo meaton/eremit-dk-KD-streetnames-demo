9eab245ab3958799e9704f414f9a596f397a5775
Mitchell John Seaton <seaton@hum.ku.dk>
2016-10-04 17:49:18 +0200

    Fixed adjust to bounds, slider enabled after tile load.

--- app/js/main.js
+++ app/js/main.js
@@ -1,34 +1,52 @@
-var mySlider = $("#fSlider").slider({ precision: 10, value: 1450 });
 var leaf_layer;
+var start_date = 1450, end_date = 1540;
+
+var mySlider = $("#fSlider").slider({ precision: 10, value: 1450, enabled: false });
+//var value = mySlider.slider('getValue');
+
+mySlider.on('slideEnabled', function() {
+  loadStreetsData();
+});
 
 mySlider.on('slideStop', function(val) {
   console.log('val: ' + val.value);
-  if (leaf_layer) leaf_layer.clearLayers();
-  if (val.value >= 1450 && val.value <= 1550) {
-    $.getJSON('/map-streets/filter/' + val.value, function(result) {
-        console.log('docs: ' + result.length);
-        $.each(result, function(i, val) {
-          console.log('no streets: ' + val.streets)
-          $.each(val.streets, function(j, street) {
-            console.log('street: ' + street.modern)
-            $.getJSON('/map-streets/location/' + street.modern, function(result) {
-              addLayer(result);
-            });
-          });
-        });
-    });
+  if (leaf_layer)
+    leaf_layer.clearLayers();
+
+  if (val.value >= start_date && val.value <= end_date) {
+    loadStreetsData(val);
   }
 });
 
-var value = mySlider.slider('getValue');
-
 var KDmap = L.map('KDmap').setView([55.678, 12.575], 13);
 
-L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWVhdG9uMnZlZ2dpZXMiLCJhIjoiY2lzcmsweTVkMDA0MTJ6bXJxenc0MDVvYSJ9.SoOKdWLPa_WDpOXJ6f_FtQ', {
+var tile = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWVhdG9uMnZlZ2dpZXMiLCJhIjoiY2lzcmsweTVkMDA0MTJ6bXJxenc0MDVvYSJ9.SoOKdWLPa_WDpOXJ6f_FtQ', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
     id: 'kd_map'
-}).addTo(KDmap);
+});
+tile.addTo(KDmap)
+tile.on('load', function(evt) {
+  if (!mySlider.slider('isEnabled'))
+    mySlider.slider('enable');
+});
+
+function loadStreetsData(val) {
+  if (val == undefined) val = { value: start_date };
+  $.getJSON('/map-streets/filter/' + val.value, function(result) {
+      console.log('docs: ' + result.length);
+      $.each(result, function(i, val) {
+        $.each(val.streets, function(j, street) {
+          console.log('street: ' + street.modern)
+          $.getJSON('/map-streets/location/' + street.modern, function(geodata) {
+            addLayer(geodata);
+            if (j == val.streets.length - 1 && i == result.length - 1)
+              KDmap.fitBounds(leaf_layer.getBounds());
+          });
+        });
+      });
+  });
+};
 
 function addLayer(data) {
   if (leaf_layer) {
@@ -49,6 +67,4 @@ function addLayer(data) {
     });
     leaf_layer.addTo(KDmap);
   }
-
-  KDmap.fitBounds(leaf_layer.getBounds());
 };
