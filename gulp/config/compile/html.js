89574d4b3e09014637221aff8809cc78271af541
Mitchell John Seaton <seaton@hum.ku.dk>
2016-10-10 18:29:31 +0200

    gulp useref upgrade

--- gulp/config/compile/html.js
+++ gulp/config/compile/html.js
@@ -3,20 +3,15 @@ var $ = require('gulp-load-plugins')();
 
 gulp.task('html', ['styles', 'fonts'], function () {
   var lazypipe = require('lazypipe');
-  var cssChannel = lazypipe()
-    .pipe($.csso);
-
-  var assets = $.useref.assets({searchPath: '{.tmp,app,.}'}); // search .tmp, app and ./bower_components for all referenced files
+  var cssChannel = lazypipe().pipe($.csso);
 
   gulp.src(['.tmp/fonts/**/*'])
     .pipe(gulp.dest('dist/fonts'));
 
   return gulp.src(['app/**/*.html'])
-    .pipe(assets)
+    .pipe($.useref({ searchPath: ['.tmp' ,'app', '.'] }))
     .pipe($.if('*.js', $.uglify()))
     .pipe($.if('*.css', cssChannel()))
-    .pipe(assets.restore())
-    .pipe($.useref())
-    .pipe($.if('*.html', $.htmlmin({processConditionalComments: true, collapseWhitespace: true})))
+    //.pipe($.if('*.html', $.htmlmin()))
     .pipe(gulp.dest('dist'));
 });
