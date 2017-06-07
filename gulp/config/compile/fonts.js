c24693c36660cb9a4ae5f251a6c3325332134d79
Mitchell John Seaton <seaton@hum.ku.dk>
2017-06-07 11:54:57 +1000

    added git repo

--- gulp/config/compile/fonts.js
+++ gulp/config/compile/fonts.js
@@ -4,9 +4,7 @@ var $ = require('gulp-load-plugins')();
 gulp.task('fonts', function () {
     return gulp.src(require('main-bower-files')()
             .concat('app/fonts/**/*')
-            
             .concat('bower_components/font-awesome/fonts/fontawesome-webfont.*')
-            
         )
         .pipe($.filter('**/*.{eot,svg,ttf,woff,woff2}'))
         .pipe($.flatten())
